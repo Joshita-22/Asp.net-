15c7176511b27563bab001a25d8cb61a20ba0259
Joshita <joshita.atmakuri@gmail.com>
2022-03-20 10:06:00 +0530

    second commit

--- WebApplication1/Controllers/PatientsController.cs
+++ WebApplication1/Controllers/PatientsController.cs
@@ -17,5 +17,59 @@ namespace WebApplication1.Controllers
             List<Patient> ptlist = hc.patient.ToList();
             return View(ptlist);
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Patient pt)
+        {
+            if (ModelState.IsValid)
+            {
+                hc.patient.Add(pt);
+                hc.SaveChanges();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            // return View(hc.patient.Single<>)
+            return View(hc.patient.Find(id));
+
+        }
+        public ActionResult Edit(Patient dc)
+        {
+            if (ModelState.IsValid)
+            {
+                hc.Entry(dc).State = System.Data.Entity.EntityState.Modified;
+                hc.SaveChanges();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(hc);
+        }
+        public ActionResult Details(int id)
+        {
+            
+            return View(hc.patient.Find(id));
+
+        }
+        public ActionResult Delete(int id)
+        {
+            return View(hc.patient.Find(id));
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeletePost(int id)
+        {
+            Patient pt = hc.patient.Find(id);
+            hc.patient.Remove(pt);
+            hc.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
